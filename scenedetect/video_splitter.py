vovamushak/PySceneDetect

commit 1cfcd5a8a693803c4e6738744c4f04474eb27493

Fix output of `save-images` and `split-video` due to new definition of end timecode.

scenedetect/video_splitter.py
@@ -155,12 +155,15 @@ def split_video_ffmpeg(input_video_paths, scene_list, output_file_template, vide
         processing_start_time = time.time()
         for i, (start_time, end_time) in enumerate(scene_list):
             duration = (end_time - start_time)
+            # Fix FFmpeg start timecode frame shift.
+            start_time -= 1
             call_list = ['ffmpeg']
             if suppress_output:
                 call_list += ['-v', 'quiet']
             elif i > 0:
                 # Only show ffmpeg output for the first call, which will display any
-                # errors if it fails. We suppress the output for the remaining calls.
+                # errors if it fails, and then break the loop. We only show error messages
+                # for the remaining calls.
                 call_list += ['-v', 'error']
             call_list += [
                 '-y',

scenedetect/cli/__init__.py
@@ -542,8 +542,8 @@ def list_scenes_command(ctx, output, filename, no_output_file, quiet):
 @click.option(
     '--rate-factor', '-crf', metavar='RATE', default=None,
     type=click.IntRange(0, 100), help=
-    'Video encoding quality (x264 constant rate factor), from 0-100. Higher indicates'
-    ' better quality at the expense of filesize, 0 indicates lossless.'
+    'Video encoding quality (x264 constant rate factor), from 0-100, where lower'
+    ' values represent better quality, with 0 indicating lossless.'
     ' [default: 22, if -h/--high-quality is set: 17]')
 @click.option(
     '--preset', '-p', metavar='LEVEL', default=None,

scenedetect/cli/context.py
@@ -192,7 +192,7 @@ class CliContext(object):
                 break
             if progress_bar:
                 progress_bar.update(1)
-            self.video_manager.seek(end_time)
+            self.video_manager.seek(end_time - 1)
             self.video_manager.grab()
             ret_val, frame_im = self.video_manager.retrieve()
             if ret_val:

commit 24956281d8cdb9c23dba80dc136aa38487066819

Fix improper start time being passed to ffmpeg.
Regressed in 1cfcd5a8a693803c4e6738744c4f04474eb27493.

Closes #93.

scenedetect/video_splitter.py
@@ -187,7 +187,7 @@ def split_video_ffmpeg(input_video_paths, scene_list, output_file_template, vide
     logging.info(
         'Splitting input video%s using ffmpeg, output path template:\n  %s',
         's' if len(input_video_paths) > 1 else '', output_file_template)
-        
+
     if len(input_video_paths) > 1:
         # TODO: Add support for splitting multiple/appended input videos.
         # https://trac.ffmpeg.org/wiki/Concatenate#samecodec
@@ -216,8 +216,6 @@ def split_video_ffmpeg(input_video_paths, scene_list, output_file_template, vide
         processing_start_time = time.time()
         for i, (start_time, end_time) in enumerate(scene_list):
             duration = (end_time - start_time)
-            # Fix FFmpeg start timecode frame shift.
-            start_time -= 1
             call_list = ['ffmpeg']
             if suppress_output:
                 call_list += ['-v', 'quiet']